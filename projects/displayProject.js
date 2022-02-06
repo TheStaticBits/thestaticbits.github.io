25c07be680fc0af8282eda79499090fbcd0e7e11
TheStaticBits <TheStaticBits@users.noreply.github.com>
2022-02-06 16:06:59 -0500

    Added transition when opening project

--- projects/displayProject.js
+++ projects/displayProject.js
@@ -1,6 +1,6 @@
-const maxOverlayOpacity = 40;
-const transProjectSpeed = 5; // Needs to be a factor of 100
-const transOverlaySpeed = 2; // Needs to be a factor of maxOverlayOpacity
+const maxOverlayOpacity = 0.4;
+const transProjectSpeed = 0.05; // Needs to be a factor of 100
+const transOverlaySpeed = 0.02; // Needs to be a factor of maxOverlayOpacity
 
 var projectOpen;
 var transitionInterval;
@@ -10,44 +10,50 @@ function transition(direction) {
     overlay = document.getElementById("overlay");
     
     if (direction === "in") {
-        if (element.style.opacity < 100) {
-            element.style.opacity += transProjectSpeed;
+        if (proj.style.opacity < 1) {
+            proj.style.opacity = parseFloat(proj.style.opacity) + transProjectSpeed;
         }
         if (overlay.style.opacity < maxOverlayOpacity) {
-            overlay.style.opacity -= transOverlaySpeed;
+            overlay.style.opacity = parseFloat(overlay.style.opacity) + transOverlaySpeed;
         }
         else {
             clearInterval(transitionInterval);
+            console.log("DONE");
         }
     } 
     
     else if (direction === "out") {
-        if (element.style.opacity > 0) {
-            element.style.opacity -= transProjectSpeed;
+        if (proj.style.opacity > 0) {
+            proj.style.opacity = parseFloat(proj.style.opacity) - transProjectSpeed;
         }
         if (overlay.style.opacity > 0) {
-            overlay.style.opacity -= transOverlaySpeed;
+            overlay.style.opacity = parseFloat(overlay.style.opacity) - transOverlaySpeed;
         }
         else {
             clearInterval(transitionInterval);
+            
+            proj.style.display = "none";
+            overlay.style.display = "none";
         }
+
+        console.log(proj.style.opacity);
+        console.log(overlay.style.opacity);
     }
 }
 
 function show(element) {
     projectOpen = element;
     var project = document.getElementById(element);
-    // project.style.opacity = 0;
+    project.style.opacity = 0;
     project.style.display = "block";
     
     var overlay = document.getElementById("overlay");
-    // overlay.style.opacity = 0;
+    overlay.style.opacity = 0;
     overlay.style.display = "block";
     
-    // transitionInterval = setInteveral(transition, 100, "in");
+    transitionInterval = setInterval(transition, 4, "in");
 }
 
 function exitProject() {
-    document.getElementById(projectOpen).style.display = "none";
-    document.getElementById("overlay").style.display = "none";
+    transitionInterval = setInterval(transition, 4, "out");
 }
